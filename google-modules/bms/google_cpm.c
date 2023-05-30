4ab3d7d09e7111e481d70adf3c8dbcc81f016d4a
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-29 19:50:57 -0700

    bms: google_cpm: Make debug_printk_prlog static
    The debug_printk_prlog variable is used in another bms driver, causing a
    linker error since both are global. Since the one in google_cpm doesn't
    need to be global, make it static to fix the linker error.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/bms/google_cpm.c
+++ google-modules/bms/google_cpm.c
@@ -293,7 +293,7 @@ struct gcpm_drv  {
 
 /* Logging ----------------------------------------------------------------- */
 
-int debug_printk_prlog = LOGLEVEL_INFO;
+static int debug_printk_prlog = LOGLEVEL_INFO;
 
 /* ------------------------------------------------------------------------- */
 
