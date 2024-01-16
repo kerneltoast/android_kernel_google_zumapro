0bab377bfaedea7e95bfeefaf57003b00ba6e820
Sultan Alsawaf <sultan@kerneltoast.com>
2024-01-15 23:45:45 -0800

    edgetpu: rio: Remove telemetry
    The telemetry logging has a massive GFP_ATOMIC allocation:
    [ 1335.572518] kworker/5:1: page allocation failure: order:4, mode:0x40a20(GFP_ATOMIC|__GFP_COMP), nodemask=(null),cpuset=/,mems_allowed=0
    [ 1335.572604] CPU: 5 PID: 19599 Comm: kworker/5:1 Tainted: G S      W         5.15.110-Sultan #1
    [ 1335.572629] Hardware name: ZUMA HUSKY MP based on ZUMA (DT)
    [ 1335.572643] Workqueue: events gcip_telemetry_worker
    [ 1335.572699] Call trace:
    [ 1335.572712] show_stack+0xb0/0xd0
    [ 1335.572754] dump_stack_lvl+0x64/0x80
    [ 1335.572799] dump_stack+0x18/0x34
    [ 1335.572830] warn_alloc+0x10c/0x184
    [ 1335.572867] __alloc_pages+0x7fc/0x1010
    [ 1335.572888] kmalloc_order+0x38/0x90
    [ 1335.572907] __kmalloc+0x1b0/0x1e0
    [ 1335.572936] gcip_telemetry_fw_log+0x64/0x250
    [ 1335.572955] gcip_telemetry_worker+0xb0/0xe0
    [ 1335.572972] process_one_work+0x1b0/0x324
    [ 1335.573000] worker_thread+0x70/0x4ac
    [ 1335.573019] kthread+0x16c/0x180
    [ 1335.573032] ret_from_fork+0x10/0x20
    [ 1335.573035] Mem-Info:
    [ 1335.573038] active_anon:216911 inactive_anon:1122137 isolated_anon:0
     active_file:135727 inactive_file:126038 isolated_file:1
     unevictable:59288 dirty:66 writeback:306
     slab_reclaimable:36313 slab_unreclaimable:113769
     mapped:279312 shmem:32482 pagetables:70320
     sec_pagetables:0 bounce:0
     kernel_misc_reclaimable:128839
     free:62665 free_pcp:1527 free_cma:192
    [ 1335.573044] Node 0 active_anon:867644kB inactive_anon:4488548kB active_file:542908kB inactive_file:504152kB unevictable:237152kB isolated(anon):0kB isolated(file):4kB mapped:1117248kB dirty:264kB writeback:1224kB shmem:129928kB writeback_tmp:0kB kernel_stack:133872kB pagetables:281280kB sec_pagetables:0kB all_unreclaimable? no
    [ 1335.573049] Normal free:250660kB min:27384kB low:257000kB high:486616kB reserved_highatomic:3168KB active_anon:867644kB inactive_anon:4488548kB active_file:542908kB inactive_file:504152kB unevictable:237152kB writepending:1016kB present:12363776kB managed:11480964kB mlocked:237152kB bounce:0kB free_pcp:6104kB local_pcp:456kB free_cma:768kB
    [ 1335.573055] lowmem_reserve[]: 0 0
    [ 1335.573059] Normal: 2271*4kB (UMEH) 15061*8kB (UMEH) 7471*16kB (UMH) 4*32kB (U) 0*64kB 0*128kB 0*256kB 0*512kB 0*1024kB 0*2048kB 0*4096kB = 249236kB
    [ 1335.573071] 680437 total pagecache pages
    [ 1335.573074] 327073 pages in swap cache
    [ 1335.573076] Swap cache stats: add 2372677, delete 2045678, find 257413/993299
    [ 1335.573078] Free swap  = 920116kB
    [ 1335.573080] Total swap = 5952956kB
    [ 1335.573081] 3090944 pages RAM
    [ 1335.573083] 0 pages HighMem/MovableOnly
    [ 1335.573085] 220703 pages reserved
    [ 1335.573087] 177152 pages cma reserved
    
    This places heavy stress on the page allocator for what is debug cruft.
    
    Properly guard the telemetry code with CONFIG_EDGETPU_TELEMETRY_TRACE and
    remove it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/edgetpu/rio/drivers/edgetpu/edgetpu-mobile-platform.c
+++ google-modules/edgetpu/rio/drivers/edgetpu/edgetpu-mobile-platform.c
@@ -32,6 +32,7 @@
 
 static struct edgetpu_dev *edgetpu_debug_pointer;
 
+#if IS_ENABLED(CONFIG_EDGETPU_TELEMETRY_TRACE)
 static void set_telemetry_mem(struct edgetpu_mobile_platform_dev *etmdev,
 			      enum gcip_telemetry_type type, struct edgetpu_coherent_mem *mem)
 {
@@ -53,6 +54,7 @@ void edgetpu_mobile_set_telemetry_mem(struct edgetpu_mobile_platform_dev *etmdev
 	set_telemetry_mem(etmdev, GCIP_TELEMETRY_LOG, etmdev->log_mem);
 	set_telemetry_mem(etmdev, GCIP_TELEMETRY_TRACE, etmdev->trace_mem);
 }
+#endif
 
 static int edgetpu_platform_setup_fw_region(struct edgetpu_mobile_platform_dev *etmdev)
 {
@@ -347,25 +349,25 @@ static int edgetpu_mobile_platform_probe(struct platform_device *pdev,
 		goto out_remove_device;
 	}
 
+#if IS_ENABLED(CONFIG_EDGETPU_TELEMETRY_TRACE)
 	etmdev->log_mem = devm_kcalloc(dev, etdev->num_cores, sizeof(*etmdev->log_mem), GFP_KERNEL);
 	if (!etmdev->log_mem) {
 		ret = -ENOMEM;
 		goto out_remove_device;
 	}
 
-#if IS_ENABLED(CONFIG_EDGETPU_TELEMETRY_TRACE)
 	etmdev->trace_mem =
 		devm_kcalloc(dev, etdev->num_cores, sizeof(*etmdev->log_mem), GFP_KERNEL);
 	if (!etmdev->trace_mem) {
 		ret = -ENOMEM;
 		goto out_remove_device;
 	}
-#endif
 
 	edgetpu_mobile_set_telemetry_mem(etmdev);
 	ret = edgetpu_telemetry_init(etdev, etmdev->log_mem, etmdev->trace_mem);
 	if (ret)
 		goto out_remove_device;
+#endif
 
 	ret = edgetpu_mobile_firmware_create(etdev);
 	if (ret) {
